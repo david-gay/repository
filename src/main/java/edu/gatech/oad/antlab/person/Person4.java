efd983349bb2438b397c52a0f61aaea76255339d
blake-harper <blakeharper@gatech.edu>
2017-01-26 22:33:06 -0500

    Implemented calc()

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Blake Harper
  *  @version 1.1
  */
 public class Person4 {
@@ -30,8 +30,12 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      StringBuilder builder = new StringBuilder();
+
+      for (int i = 0; i < input.length(); i++) {
+        builder.append((char)(input.charAt(i)+1));
+      }
+      return builder.toString();
     }
     
     /**
